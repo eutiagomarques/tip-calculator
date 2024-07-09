0a7f806dd199cf67ad34a66fae465233d8540c55
Tiago Marques <ttsmarques@gmail.com>
2024-07-09 18:16:46 -0300

    setup root programmatically

--- tip-calculator/MainViewController.swift
+++ tip-calculator/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  tip-calculator
 //
 //  Created by Tiago de Souza Marques on 09/07/24.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
-
